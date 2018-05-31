5624603ad7db4969987889b55a52f47e52ab9c27
Shunsuke Miyoshi <s.miyoshi@jp.fujitsu.com>
2018-05-31 07:21:16 +0000

    add script of making temp tls secret

---
+++ kubernetes/make_secret.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+tmpdir=$(mktemp -d)
+cd $tmpdir
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/C=JP"
+
+crt_value=`base64 tls.crt | tr -d '\n'`
+key_value=`base64 tls.key | tr -d '\n'`
+
+cd -
+
+cat << EOF > secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: frontend-tls
+  namespace: default
+type: kubernetes.io/tls
+data:
+  tls.crt: ${crt_value}
+  tls.key: ${key_value}
+EOF
+
+mkdir -p istio
+cat << EOF > istio/secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: istio-ingress-certs
+  namespace: istio-system
+type: kubernetes.io/tls
+data:
+  tls.crt: ${crt_value}
+  tls.key: ${key_value}
+EOF
