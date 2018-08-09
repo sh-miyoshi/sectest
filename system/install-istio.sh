b518bdcc323d5aa459eb5e98b3470c014240fed9
Shunsuke Miyoshi <s.miyoshi@jp.fujitsu.com>
2018-08-09 02:44:37 +0000

    remove useless download

--- system/install-istio.sh
+++ system/install-istio.sh
@@ -10,14 +10,19 @@ fi
 ISTIO_VERSION="1.0.0"
 
 NAME="istio-$ISTIO_VERSION"
-URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
-echo "Downloading $NAME from $URL ..."
-curl -L "$URL" | tar xz
-# TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
-echo "Downloaded into $NAME:"
-BINDIR="$(cd $NAME/bin; pwd)"
 
-\cp -f $BINDIR/istioctl /usr/local/bin/
+ls istio-* > /dev/null 2>&1
+# if istio data is not download yet
+if [ $? != 0 ]; then
+  URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+  echo "Downloading $NAME from $URL ..."
+  curl -L "$URL" | tar xz
+  # TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
+  echo "Downloaded into $NAME:"
+  BINDIR="$(cd $NAME/bin; pwd)"
+
+  \cp -f $BINDIR/istioctl /usr/local/bin/
+fi
 
 # fix helm chart template bug
 cd $NAME/install/kubernetes/helm/istio/templates/
