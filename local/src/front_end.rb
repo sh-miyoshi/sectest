c9d5f7e5819fb53b00b21cb88d7fb5e52244adef
s.miyoshi <s.miyoshi1991@gmail.com>
2019-02-17 17:03:08 +0900

    add admin api

--- local/src/front_end.rb
+++ local/src/front_end.rb
@@ -12,22 +12,22 @@ get '/' do
 end
 
 post '/data' do
-  uri = "http://" + ENV['API_SERVER_URL'] + ":4567/api"
+  uri = 'http://' + ENV['API_SERVER_URL'] + ':4567/api'
   url = URI.parse(uri)
 
   req = Net::HTTP::Post.new(url.request_uri)
   req.body = {
-    "user" => params[:user],
-    "password" => params[:password]
+    'user' => params[:user],
+    'password' => params[:password]
   }.to_json
   res = Net::HTTP.start(url.host, url.port) do |http|
     http.request(req)
   end
-  if (200 <= res.code.to_i && res.code.to_i < 300) then
+  if res.code.to_i >= 200 && res.code.to_i < 300
     @val = JSON.parse(res.body)
   else
-    @val = Array.new
-    @val.push("Error")
+    @val = []
+    @val.push('Error')
   end
 
   erb :index
---
+++ local/src/mysql_api_server.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'mysql2'
+require 'json'
+
+set :bind, '0.0.0.0'
+
+post '/api', provides: :json do
+  params = JSON.parse(request.body.read)
+  begin
+    mysql = Mysql2::Client.new(
+      username: params['user'],
+      password: params['password'],
+      database: 'secret',
+      host: ENV['MYSQL_ADDR']
+    )
+    data = []
+    mysql.query('select * from info').each do |row|
+      data << row
+    end
+    return data.to_json
+  rescue StandardError => e
+    p e.message
+    status 400
+  end
+end
+
+get '/admin/info' do
+  return {
+    "info": 'admin user info'
+  }.to_json
+end
