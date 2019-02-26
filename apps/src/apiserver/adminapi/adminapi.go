8123573c53640cedecc8c8ac5613bc22e0dc93b5
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-02-26 05:18:38 +0000

    fix admin message

--- apps/src/apiserver/adminapi/adminapi.go
+++ apps/src/apiserver/adminapi/adminapi.go
@@ -1,14 +1,25 @@
 package adminapi
 
 import (
+	"json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type AdminInfoResponse struct {
+	Message string `json:"msg"`
+}
+
 func handlerInfoAPI(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("ok"))
+	data := AdminInfoResponse{
+		Message: "admin value",
+	}
+	res, err := json.Marshal(data)
+	log.Printf("response data: %v\n", data)
+
+	w.Write(res)
 }
 
 // AdminServer runs for admin API
